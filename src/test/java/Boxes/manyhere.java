cb9d7fac890fc298e3af6906431bc8624dc07d74
IsmaelStevenVenegasBalaguera <istevenvb@gmail.com>
2025-04-01 23:04:02 +0200

    Fifth commit

---
+++ src/test/java/Boxes/manyhere.java
@@ -0,0 +1,9 @@
+package Boxes;
+
+public class manyhere {
+    public static void main(String[] args) {
+        String name= "Julio";
+        String lastName= "Flores";
+        System.out.println(name+" "+lastName);
+    }
+}
